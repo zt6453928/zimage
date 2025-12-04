55ed2f9dda6e48b8113334a3cdbdba4804b64dc0
zt6453928 <zt6453928@github.com>
2025-12-04 22:27:33 +0800

    Update Vite config for GitHub Pages support

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: process.env.NODE_ENV === 'production' ? '/zimage/' : '/',
   server: {
     host: '127.0.0.1',
     port: 3000
